1f8ec59781e9f8de7e1971fb61247a7da0fb95e7
nashbateman <33039086+nashbateman@users.noreply.github.com>
2019-04-11 18:15:34 -0500

    New variable updates

--- java classes/NewSetClicked.java
+++ java classes/NewSetClicked.java
@@ -8,13 +8,32 @@ import android.view.View;
 import android.widget.Toast;
 
 public class NewSetClicked extends AppCompatActivity {
+    EditText setName;
+    EditText numCards;
+    flashCard deckList;
+    String deckNames[];
+    String deckName;
+    int numberOfCards;
+    
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newset);
-
-
+        setName = (EditText) FindViewById(R.id.setName);
+        numCards = (EditText) FindViewById(R.id.numCards);
+        deckName = setName;
+        numvberOfCards = numCards;
+        createNewDeck(deckName, numberofCards);
     }
+    public void createNewDeck(String deckName, int numberOfCards) {
+        Decks deck = new Deck();
+        deck.createDeck(deckName, numberOfCards);
+        deckList.add(deck);
+        deckNames.add(deckName);
+    }
+        
     public void nextClick(View v){
         Toast.makeText(this, "View / Delete button clicked",
                 Toast.LENGTH_SHORT).show();
