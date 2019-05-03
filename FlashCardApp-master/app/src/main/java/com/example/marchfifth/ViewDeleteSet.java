030704b59f4755e60cf5449e9359344f8fb62ca2
nashbateman <33039086+nashbateman@users.noreply.github.com>
2019-05-02 19:55:26 -0500

    added javaDocs

--- FlashCardApp-master/app/src/main/java/com/example/marchfifth/ViewDeleteSet.java
+++ FlashCardApp-master/app/src/main/java/com/example/marchfifth/ViewDeleteSet.java
@@ -25,12 +25,24 @@ public class ViewDeleteSet extends AppCompatActivity {
     Button viewButton;
     Button deleteButton;
     EditText SetName;
-
+     /**
+    * onCreate method called when app is created, sets content view and EditText 
+    * variables for user input
+    * @param Bundle savedInstanceState saved instance state of app in current condition
+    * @return nothing
+    * @since 1.0
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewdeleteset);
-
+        /**
+        * on click listener is set for viewButton, when button is pressed 
+        * name of set wishing to be viewed is set and method is called to change content view to view set 
+        * @param on click listener of view
+        * @return nothing
+        * @since 1.0
+        */
         SetName = (EditText) findViewById(R.id.SetName);
         viewButton = (Button) findViewById(R.id.ViewButton);
         viewButton.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +52,13 @@ public class ViewDeleteSet extends AppCompatActivity {
                 openNextActivity1(set);
             }
         });
-
+        /**
+        * on click listener is set for deleteButton, when button is pressed 
+        * name of set wishing to be deleted is set and method is called to change content view 
+        * @param on click listener of view
+        * @return nothing
+        * @since 1.0
+        */
         deleteButton = (Button) findViewById(R.id.Delete);
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,13 +70,25 @@ public class ViewDeleteSet extends AppCompatActivity {
         });
 
     }
-
+    /**
+    * method to change content view to view the deck being requested
+    * intent is used to pass name of the deck and to start next activity
+    * @param set name of the set wishing to be viewed
+    * @return nothing
+    * @since 1.0
+    */
     public void openNextActivity1(String set) {
         Intent intent = new Intent(this, viewCard.class);
         intent.putExtra(SET, set);
         startActivity(intent);
     }
-
+    /**
+    * method to change content view to main menu after deck has been deleted
+    * intent is used to start next activity
+    * @param nothing
+    * @return nothing
+    * @since 1.0
+    */
     public void openNextActivity2() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
